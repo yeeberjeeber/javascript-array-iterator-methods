f3f04272d82f3c71ab352e3e21172dc23d375a2d
Aloysius <aloysius.tan00@gmail.com>
2026-01-01 21:41:31 +0800

    Level Up Exercise 2

--- app.js
+++ app.js
@@ -410,3 +410,46 @@ totalYearsLived = inventors.reduce(function(acc, inventor) {
 // Check your work:
 console.log('Level Up 1 my result: ', totalYearsLived);
 console.log('Level Up 1 correct result: ', 861);
+
+/*
+Level Up exercise 2: Array.prototype.reduce()
+
+Tallying travel methods using Array.prototype.reduce(). 
+
+Count the number of times each travel method appears in the 'travelMethods'
+array.
+
+- The resulting object should have keys as the travel methods 
+  ('car', 'truck', 'bike', etc.) and values as their respective counts.
+- Store this object in the variable 'travelMethodCounts'.
+
+Hints:
+- Inside the reduce function, check if the travel method already exists as a key
+  in your accumulator object. If it does, increment its count. If not, add it 
+  to the object and give it a value of 1.
+- Since you want to return an object, be sure to pass an empty {} for the 
+  initial value of the "accumulator".
+*/
+
+let travelMethodCounts = {};
+
+// Complete the exercise in the space below:
+travelMethodCounts = travelMethods.reduce(function(acc, travel) {
+    if(acc[travel]){
+        acc[travel] += 1;
+    }
+    else {
+        acc[travel] = 1;
+    }
+    return acc;
+}, {})
+
+// Check your work:
+console.log('Level Up 2 my result: ', travelMethodCounts);
+console.log('Level Up 2 correct result: ', {
+  car: 5,
+  truck: 3,
+  bike: 2,
+  walk: 2,
+  van: 2,
+});
