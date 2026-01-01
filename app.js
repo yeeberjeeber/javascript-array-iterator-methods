a5729e04ea6dea49df0ac5f59b0a1116fe9c8757
Aloysius <aloysius.tan00@gmail.com>
2026-01-01 21:32:57 +0800

    Exercise 8

--- app.js
+++ app.js
@@ -337,3 +337,23 @@ isEveryone19OrOlder = devs.every((person) => {
 // Check your work:
 console.log('Exercise 7 my result: ', isEveryone19OrOlder);
 console.log('Exercise 7 correct result: ', false);
+
+/*
+Exercise 8: Array.prototype.find()
+
+Use Array.prototype.find() to identify and retrieve the comment object with 
+a specific ID 823423 from an array of comment objects.
+
+- Assign the found comment object to the variable 'commentById'.
+*/
+
+let commentById = {};
+
+// Complete the exercise in the space below:
+commentById = comments.find((comment) => {
+    return comment.id === 823423;
+})
+
+// Check your work:
+console.log('Exercise 8 my result: ', commentById);
+console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 });
